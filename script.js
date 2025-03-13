46c74f35cb2f484a27184161c65dcbe1b6b6c8b5
M1N00 <antoni.debaz@gmail.com>
2025-03-13 19:55:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+//ICI C TOUT LE BORDEL AVEC STRIPE
+
 const button = document.getElementById('ACHETER');
 
 button.addEventListener('click', () => {
   //Ceci redirige l'utilisateur vers la page de paiement
    window.location.href = 'https://buy.stripe.com/6oEg06duo1N49BSdQQ';
 });
+
+
