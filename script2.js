08bd657e866cf40d60c59bb43282603f4f894b01
M1N00 <antoni.debaz@gmail.com>
2025-03-22 19:09:33 +0100

    Add files via upload

--- script2.js
+++ script2.js
@@ -35,7 +35,7 @@ Upload.addEventListener('click', () => {
         }
 
         const file = fileInput.files[0];
-        const storageRef = ref(storage, `uploads/${localStorage.getItem("userEmail")}/${sessionId}/${document.getElementById("adresse").value}, (,${new Date().toLocaleDateString().replace(/\//g, ' ')},),/${file.name}`); // Stocke le fichier dans le dossier "uploads" puis dans un dossier spécifique à l'utilisateur
+        const storageRef = ref(storage, `uploads/${localStorage.getItem("userEmail")}/${sessionId}/${document.getElementById("adresse").value} (${new Date().toLocaleDateString().replace(/\//g, ',')})/${file.name}`); // Stocke le fichier dans le dossier "uploads" puis dans un dossier spécifique à l'utilisateur
         
         if(document.getElementById("adresse").value != "") {
         
